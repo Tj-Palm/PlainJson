2553ccb4c94038acaebabc86a3d3b202e4156e06
Tj-Palm <48101800+Tj-Palm@users.noreply.github.com>
2020-02-23 19:36:59 +0100

    Added AutoSale class with props

---
+++ ModelLib/AutoSale.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelLib
+{
+    class AutoSale
+    {
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public List<Car> Cars { get; set; }
+    }
+}
